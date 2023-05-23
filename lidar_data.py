92f6a98c376cde22ee0e94673c57f91b6e61adde
PrajwalBG1999 <prajwalgaddigoudar99@gmail.com>
2023-05-23 09:39:54 -0500

    Initial commit

---
+++ lidar_data.py
@@ -0,0 +1,27 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+
+class LidarSubscriber(Node):
+    def __init__(self):
+        super().__init__('lidar_subscriber')
+        self.subscription = self.create_subscription(
+            LaserScan,
+            '/scan',
+            self.lidar_callback,
+            10
+        )
+        self.subscription
+
+    def lidar_callback(self, msg: LaserScan):
+        for i, r in enumerate(msg.ranges):
+            print(f"Range[{i}]: {r}")
+def main(args=None):
+    rclpy.init(args=args)
+    lidar_subscriber = LidarSubscriber()
+    rclpy.spin(lidar_subscriber)
+    lidar_subscriber.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
